6b44a47e48b0cab0e50be99bee3e2b2cc94324d4
epstomai <knia@live.cn>
2019-03-10 23:24:29 +0800

    add module of getExEquipSkills

--- src/main.lua
+++ src/main.lua
@@ -4,62 +4,86 @@
 --- DateTime: 2019/3/2 20:24
 ---
 require('utils')
+require("ex_skill_names")
 require("xml_skill_skills")
 require("xml_skill_skillgroup")
 require("xml_skill_formulas")
-require("ex_skill_names")
 require("xml_skill_combo")
 require("xml_equip_skill")
---90101
+require('xml_fight_conf')
+require('xml_skill_effects')
+require("xml_formula")
+
 --获取组合技列表
 --require("getComboList")
 --runGetCombo()
 
 --获取刻印特技列表
---getExEquipSkill()
---local name = CfgControlSkillGroup:getSkillName(90101)
---require('getExEquipSkills')
-local ex_skill_names = Ex_skill_names.ex_skill_names
-local equip_skills = XML_equip_skill.equip_skill
-local ex_skills_list = {}
---获取当前刻印特技显示列表，包含每级共鸣技能groupid和数值乘数
-for equip_skills_i, equip_skills_v in pairs(equip_skills) do
-    for ex_skill_names_i, ex_skill_names_v in pairs(ex_skill_names) do
-        if equip_skills_v.name == ex_skill_names_v then
-            table.insert(ex_skills_list,equip_skills_v)
+function getExSkillList()
+    local ex_skill_names = Ex_skill_names.ex_skill_names
+    local equip_skills = XML_equip_skill.equip_skill
+    local ex_skills_list = {}
+    --获取当前刻印特技显示列表，包含每级共鸣技能groupid和数值乘数
+    for equip_skills_i, equip_skills_v in pairs(equip_skills) do
+        for ex_skill_names_i, ex_skill_names_v in pairs(ex_skill_names) do
+            if equip_skills_v.name == ex_skill_names_v then
+                table.insert(ex_skills_list,equip_skills_v)
+            end
         end
     end
-end
-local eq_skill_key
-
-for i, v in pairs(ex_skills_list) do
-
+    return ex_skills_list
 end
 
+--获得每个技能每级共鸣的描述(一条描述)
+function getEachLevelDesc(value)
+    local group_id = value[2]
+    local level = value[3]
+    --搬运的时候需要html标签就去掉这个方法{{!}}
+    --local desc = parseDesc(group_id,nil,level)
+    local desc = removeHtmlMark(parseDesc(group_id,nil,level))
+    return desc
+end
 
-function getEachSkillDesc(ex_skill)
+--获得每个技能的全部描述（四条描述）
+function getEachSkillAllDesc(ex_skill)
     local desc_table = {}
     local zone = ex_skill.zone
-    local group_id_list = {}
 
     --取出 +0 +2 +5 +7 级共鸣参数
     local key
+    local desc_obj = {}
     local switch = {
         [1] = function(v)
             --处理共鸣0时的描述，并插入到描述表里
-            print(1)
+            desc_obj = {
+                ["id"] = v[2],
+                ["desc"] = getEachLevelDesc(v)
+            }
+            table.insert(desc_table,1,desc_obj)
         end,
         [3] = function(v)
             --处理共鸣2时的描述，并插入到描述表里
-            print(2)
+            desc_obj = {
+                ["id"] = v[2],
+                ["desc"] = getEachLevelDesc(v)
+            }
+            table.insert(desc_table,2,desc_obj)
         end,
         [6] = function(v)
             --处理共鸣5时的描述，并插入到描述表里
-            print(3)
+            desc_obj = {
+                ["id"] = v[2],
+                ["desc"] = getEachLevelDesc(v)
+            }
+            table.insert(desc_table,3,desc_obj)
         end,
         [8] = function(v)
             --处理共鸣7时的描述，并插入到描述表里
-            print(4)
+            desc_obj = {
+                ["id"] = v[2],
+                ["desc"] = getEachLevelDesc(v)
+            }
+            table.insert(desc_table,4,desc_obj)
         end,
     }
     local fSwitch = switch[key]
@@ -67,47 +91,146 @@ function getEachSkillDesc(ex_skill)
         v = table_arrange(v)
         key = v[1]
         fSwitch = switch[key]
-        print('key:'..key)
         if fSwitch then
-            local result = fSwitch(v)
-        else
-
+            fSwitch(v)
         end
     end
-end
-
-function getEachLevelDesc(value)
-    local key = value[1]
-    local group_id = value[2]
-    local level = value[3]
-    local desc = ""
-    local fix_percent
-    local fix_level
-    local skill_formulas = XML_skill.formulas
-    local skill_groups = XML_skill.skillgroup
-    desc = removeHtmlMark(skill_groups[group_id].full_desc)
-    --寻找描述中formula表的匹配，可能会匹配到多个
-    local match_ids = {}
-    for match in string.gmatch(desc,'{[%a%A]-}') do
-        table.insert(match_ids,StrToTable(match).id)
-    end
-    --将匹配对应到formula表中具体的数值
-    for k, id in ipairs(match_ids) do
-        fix_level = skill_formulas[id].fix_level
-        fix_percent = skill_formulas[id].fix_percent
-        --TODO:计算值并写入
-    end
-
-    --重新写入描述并返回
-    for i, v in ipairs(value) do
-    table_ret[i] = string.gsub(str,match,tostring(v..'%%'))
-    end
+    return desc_table
 end
 
 
-getEachSkillDesc(ex_skills_list[2])
+--下面两个是官方方法
+local calFormulaValue =
+function(groupid, fomularid, heroData, skillLv)
+    local zt_context = {}
+    local fid = XML_skill.formulas[fomularid].fid
+    --cclog("fid = " .. fid)
+
+    local formula = XML_formula.fids[fid].formula_view
+
+    local src_hpc = heroData and heroData:getHp() or 0
+    local src_hp = heroData and heroData:getHp() or 0
+    local src_gj = heroData and heroData:getAttack() or 0
+    local src_fy = heroData and heroData:geDef() or 0
+    local src_bj = heroData and heroData:getCrit() or 0
+    local src_fb = heroData and heroData:getFangCrit() or 0
+    local src_ah = heroData and heroData:getAddHurt() or 0
+    local src_sh = heroData and heroData:getReduceHurt() or 0
+    local src_level = heroData and heroData:getLevel() or 0
+    local src_slevel = skillLv
+    if src_slevel == nil then
+        src_slevel = heroData and heroData:getSkillLevel(groupid) or 1
+    end
+    src_slevel = math.max(1, src_slevel)
+
+    table.insert(zt_context, "local src_hpc = " .. src_hpc)
+    table.insert(zt_context, "local src_hp = " .. src_hp)
+    table.insert(zt_context, "local src_gj = " .. src_gj)
+    table.insert(zt_context, "local src_fy = " .. src_fy)
+    table.insert(zt_context, "local src_bj = " .. src_bj)
+    table.insert(zt_context, "local src_fb = " .. src_fb)
+    --·À±¬»÷
+    table.insert(zt_context, "local src_ah = " .. src_ah)
+    table.insert(zt_context, "local src_sh = " .. src_sh)
+    table.insert(zt_context, "local src_level = " .. src_level)
+    table.insert(zt_context, "local src_slevel = " .. src_slevel)
+
+    table.insert(zt_context, "local cf_slevel = " .. XML_skill.formulas[fomularid].slevel)
+    table.insert(zt_context, "local cf_fix_p = " .. XML_skill.formulas[fomularid].fix_percent)
+    table.insert(zt_context, "local cf_fix_level = " .. XML_skill.formulas[fomularid].fix_level)
+    table.insert(zt_context, "local cf_add = " .. XML_skill.formulas[fomularid].add_value)
+    table.insert(zt_context, "local cf_add_level = " .. XML_skill.formulas[fomularid].add_level)
+
+    --    table.insert(zt_context, "cclog('cf_fix_p = ' .. tostring(cf_fix_p))")
+    --    table.insert(zt_context, "cclog('cf_fix_level = ' .. tostring(cf_fix_level))")
+    --    table.insert(zt_context, "cclog('src_slevel = ' .. tostring(src_slevel))")
+
+    --cclog("formula = " .. tostring(formula))
+    if formula then
+        table.insert(zt_context, "return " .. tostring(formula))
+    end
+
+    local zs_ctx = table.concat(zt_context, "\n")
+    local func = loadstring(zs_ctx)
+    local zb_ret, zs_errmsg = pcall(func)
+    --cclog("zb_ret = " .. tostring(zb_ret) .. " zs_errmsg = " .. tostring(zs_errmsg))
+    if not zb_ret then
+        return 0
+    end
+    return tonumber(zs_errmsg) or 0
+end
+
+function parseDesc(id, heroData, skillLv)
+    if nil == id or nil == XML_skill.skillgroup[id] then
+        return
+    end
+    local desc = XML_skill.skillgroup[id].full_desc
+    if desc then
+        local pattern = "{.-}"
+        -- 转化数值
+        desc =
+        string.gsub(
+                desc,
+                pattern,
+                function(s)
+                    --cclog("s = " .. s)
+                    local obj = loadstring("return " .. s)()
+                    local effectid = obj.id
+
+                    local effect_list = XML_skill.effects[effectid].effect_list
+                    local effectObj = effect_list[obj.index]
+                    local effectValue = 0
+                    if effectObj.buff_pkg then
+                        local buffs = effectObj.buffs or {}
+                        local pkg_index = obj.pkgindex or 1
+                        local pkg_obj = buffs[pkg_index] or {}
+                        --tst_print_lua_table(pkg_obj)
+                        effectValue = pkg_obj.main_value or 0
+                        if effectValue == 0 then
+                            --cclog("buff_pkg effectObj = " .. gf_TableToString(effectObj))
+                        end
+                        effectObj = pkg_obj
+                    else
+                        effectValue = effectObj.main_value or 0
+                        if effectValue == 0 then
+                            --cclog("effectObj = " .. gf_TableToString(effectObj))
+                        end
+                    end
+
+                    local value = 0
+                    if XML_fight_conf.enames[effectObj.main_type] then
+                        value = calFormulaValue(id, effectValue, heroData, skillLv)
+                    else
+                        value = effectValue
+                    end
+
+                    if obj.format == 1 then
+                        value = math.floor(value)
+                    elseif obj.format == 2 then
+                        value = tonumber(value)
+                        value = value * 100
+                        value = math.floor(value + 0.5)
+                        value = string.format("%d%%", value)
+                    end
+                    return value
+                end
+        )
+    end
+    return desc
+end
 
 
-
-
-
+--运行获得刻印列表
+local ex_skills_list = getExSkillList()
+local skillgroup = XML_skill.skillgroup
+local desc_list = {}
+local skillname
+local output_list = {}
+local output_list_obj = {}
+for key, ex_skill in pairs(ex_skills_list) do
+    skillname = skillgroup[ex_skill.zone[1][2]].name
+    desc_list = getEachSkillAllDesc(ex_skill)
+    for i, desc_obj in pairs(desc_list) do
+        print(desc_obj.id..","..skillgroup[desc_obj.id].name..","..desc_obj.desc)
+    end
+end
